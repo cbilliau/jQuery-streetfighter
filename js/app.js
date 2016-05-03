820e391bfdbcf8c1f604603641bdcf0183537e33
cbilliau <christopher@billiau.com>
2016-05-03 14:31:19 -0400

    Add sound to .js

--- js/app.js
+++ js/app.js
@@ -8,15 +8,25 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function()	{
-		console.log('mousedown');
-		// play haudoken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// animate had to right of scrn
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function()	{
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
 	})
 	.mouseup(function()	{
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+function playHadouken ()	{
+	$('#hadouken-sound')[0].volume= 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
